65ce66b3f6c68c357223a0e86b17ecbe4669407e
Terry <53505355+TerryJoo@users.noreply.github.com>
2023-01-05 10:47:25 +0900

    Modify pg plugin for email masking

--- pg_plugins/update_for_stage/index.sql
+++ pg_plugins/update_for_stage/index.sql
@@ -1,12 +1,12 @@
 START TRANSACTION;
 UPDATE "user" u1
-SET "phone_number" = '010XXXXXXXX', "email" = CONCAT(u2.email, '.TEST')
+SET "phone_number" = '010XXXXXXXX', "email" = REGEXP_REPLACE(u2.email, '.....$', 'XXXXX')
 FROM "user" u2
 WHERE u1.user_id = u2.user_id
     AND u1."phone_number"
     NOT IN ('010EXAMPLE');
 UPDATE "customer" c1
-SET "phone" = '010XXXXXXXX', "email" = CONCAT(c2.email, '.TEST')
+SET "phone_number" = '010XXXXXXXX', "email" = REGEXP_REPLACE(u2.email, '.....$', 'XXXXX')
 FROM "customer" c2
 WHERE c1.user_id = c2.user_id
     AND c1."phone"
