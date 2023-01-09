64bfbdc35740a18a50d9021561f4f020dafe912f
Terry <53505355+TerryJoo@users.noreply.github.com>
2023-01-09 09:43:38 +0900

    Update index.sql

--- pg_plugins/update_for_stage/index.sql
+++ pg_plugins/update_for_stage/index.sql
@@ -6,7 +6,7 @@ WHERE u1.user_id = u2.user_id
     AND u1."phone_number"
     NOT IN ('010EXAMPLE');
 UPDATE "customer" c1
-SET "phone" = '010XXXXXXXX', "email" = REGEXP_REPLACE(u2.email, '.....$', 'XXXXX')
+SET "phone" = '010XXXXXXXX', "email" = REGEXP_REPLACE(c2.email, '.....$', 'XXXXX')
 FROM "customer" c2
 WHERE c1.user_id = c2.user_id
     AND c1."phone"
